cf7695500e64ef0494d1600cac5b321642123e67
Artyom Romanov <artem.romanov.03@bk.ru>
2023-11-24 16:51:03 +0500

    Add process time header Middleware
    Signed-off-by: Artyom Romanov <artem.romanov.03@bk.ru>

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,8 @@
+import time
+from typing import Callable
+
 import uvicorn
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, Request
 
 from src.app.internal.core.auth.transport.handlers import auth_router
 from src.app.internal.users.transport.handlers import users_router
@@ -19,6 +22,15 @@ app = FastAPI(
 )
 
 app.include_router(api_router)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next: Callable):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 @app.get("/")
